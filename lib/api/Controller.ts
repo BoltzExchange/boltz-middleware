2a6fdcbb394a3cc3bf7b9f1ee7a3914d45df9f66
michael1011 <me@michael1011.at>
2019-01-26 11:32:12 +0100

    fix: add X-Accel-Buffering header (#39)

--- lib/api/Controller.ts
+++ lib/api/Controller.ts
@@ -100,6 +100,7 @@ class Controller {
       res.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
+        'X-Accel-Buffering': 'no',
         Connection: 'keep-alive',
       });
 
